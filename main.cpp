9b805a1019a1fd87e75c442d4db6d4176fa24e41
Will-Cupa <77106918+Will-Cupa@users.noreply.github.com>
2025-03-03 10:15:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,14 @@
 using Namespace std;
 
 int main(){
+  ifstream file;
+
+  string word;
+  while(file >> word){
+    if(word.find(".text")){
+      file >> word;
+      cout << word << endl;
+  }
+  
   return 0;
 }
